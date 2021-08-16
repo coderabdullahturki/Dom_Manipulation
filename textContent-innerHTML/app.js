d7ca85a62d7665f870fa56bfe8773193f92e6a48
Abdullah Turky <abdullahturki087@gmail.com>
2021-08-16 21:53:29 +0600

    textContentt and innerHTML

---
+++ textContent-innerHTML/app.js
@@ -0,0 +1,20 @@
+const list = document.getElementById('list');
+const header = document.getElementById('header');
+const item = document.getElementById('item');
+
+console.log(header.textContent);
+console.log(list.textContent);
+console.log(list.innerHTML);
+
+const randomVar = 'Any value';
+
+const ul = document.createElement('ul');
+
+ul.innerHTML = `<li class="item">${randomVar}</item>
+                <li>list item</li>
+                <li>list item</li>`;
+
+ document.body.appendChild(ul); 
+ 
+ 
+ header.textContent = `<h2>added text through textContent</h2>`;
